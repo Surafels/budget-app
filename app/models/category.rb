92c420bc40b12817b8c12845f396d1dc017d71ee
Ssurafels <ssurafel4@gmail.com>
2024-01-30 10:43:05 +0300

    Validates Item and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
     belongs_to :user, foreign_key: "author_id"
     has_many :items
+
+    validates :name, presence :true length: { minimum: 2, maximum: 50 }
+    validates :icon, precence :true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  validates :amount, precence :true
+  validates :name, precence :true length: { minimum: 2, maximum: 50 }
+
 end
