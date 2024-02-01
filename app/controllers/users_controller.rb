e6facd797056025fbc3f781372571f20c0706c3b
Ssurafels <ssurafel4@gmail.com>
2024-02-01 23:08:14 +0300

    Remove unnecessary code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,6 @@
 class UsersController < ApplicationController
   # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
-  def show; end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit; end
+  def index; end
 
   private
 
