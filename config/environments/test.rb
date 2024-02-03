b56e33f1f5d751cd56084a3a46a7289c8d78cbd5
Ssurafels <ssurafel4@gmail.com>
2024-02-03 15:52:56 +0300

    Update puma.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,7 +18,8 @@ Rails.application.configure do
   config.eager_load = ENV["CI"].present?
 
   # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
   config.public_file_server.headers = {
     "Cache-Control" => "public, max-age=#{1.hour.to_i}"
   }
--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,7 @@ port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 
-# workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 preload_app!
 
