1ee707b44dfec2b0d7c6c54975dc26a55f17af23
Ssurafels <ssurafel4@gmail.com>
2024-02-03 19:45:48 +0300

    Update render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,17 @@
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+./bin/rails assets:precompile
+./bin/rails assets:clean
 bundle exec rake db:migrate
